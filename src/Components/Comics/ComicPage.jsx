9039e4365480539c4e4a31f55038a16344d4a32e
KeithLord <ismaelcerrudo.12@gmail.com>
2022-11-04 15:59:50 -0300

    ComicPagee

--- src/Components/Comics/ComicPage.jsx
+++ src/Components/Comics/ComicPage.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import {useEffect} from 'react'
 
 function ComicPage() {
     let {id} = useParams()
-    console.log(id)
+
+    const getComicById = async ()=>{
+        const response = await fetch(`https://gateway.marvel.com:443/v1/public/comics/${id}?apikey=67fab671b7006d9a1f390f9ff7c7abb2`);
+        const data = await response.json();
+        console.log(data);
+    }
+useEffect(() => {
+  getComicById();
+
+}, [])
+
   return (
     <div>ComicPage</div>
   )
